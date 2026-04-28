30e46b98cbea88fd77ae6466eec0cea28afe6fcd
seadarknesk <bogdanbontos105@gmail.com>
2026-04-28 20:45:53 +0300

    First commit: WIP

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame_time = pygame.time.Clock()
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        dt = pygame_time.tick(60) / 1000
+if __name__ == "__main__":
+    main()
